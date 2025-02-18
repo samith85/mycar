0b3985ca43cac39a43bb1e16afd38ac50bc1f26c
samith85 <41486503+samith85@users.noreply.github.com>
2025-02-18 20:52:41 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+let spd = 0
+spd += 5
+MiniCar.motor(Motorlist.M1, Direction1.Forward, spd)
 basic.forever(function () {
-	
+    MiniCar.led_rgb(LED_rgb_L_R.LED_R, LED_color.red1)
+    basic.pause(500)
+    MiniCar.led_rgb(LED_rgb_L_R.LED_R, LED_color.black)
+    MiniCar.led_rgb(LED_rgb_L_R.LED_L, LED_color.green1)
+    basic.pause(500)
+    MiniCar.led_rgb(LED_rgb_L_R.LED_L, LED_color.black)
 })
